596f2e1c780e6aa2c3e6030b03b4e5201a0b752f
Loke <pakyenloke@gmail.com>
2023-05-27 14:42:41 +0800

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path='project1' element={<Project1/>} />
-        <Route path='project2' element={<Project2/>} />
-        <Route path='project3' element={<Project3/>} />
-        <Route path='resume' element={<Resume/>} />
+        <Route path='/project1' element={<Project1/>} />
+        <Route path='/project2' element={<Project2/>} />
+        <Route path='/project3' element={<Project3/>} />
+        <Route path='/resume' element={<Resume/>} />
       </Routes>
       </BrowserRouter>
       )
